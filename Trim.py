a7eafaa74af6e3f77e00b4324d05275fda0fb8a5
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2022-12-25 16:30:58 +0530

    progress

--- Trim.py
+++ Trim.py
@@ -3,6 +3,8 @@ import random
 
 # Read the CSV file
 df = pd.read_csv('data.csv')
+new = [1, 2, 3]
+#df = df.append(pd.Series(new, index=df.columns[:len(new)]), ignore_index=True)
 
 # Print the dataframe
 num_rows = df.shape[0]
@@ -14,23 +16,25 @@ for i in range(num_rows):
     flag = 0
     if(high_val > 1000):
         while flag<3:
+            print("wassup")
             randn = random.randint(1, 2*count)
             x = random.randint(1, 2*count - randn)
             start = df.iloc[i, randn]
             end = df.iloc[i, randn + x]
             if(end - start > 64):
-                df.iloc[i, randn] = start + 32
-                df.iloc[i, randn + x] = end - 32
+
                 flag += 1
             else:
                 continue
     elif(high_val > 500 and high_val < 1000):
         while flag<2:
+            print("hohohoho")
             randn = random.randint(1, count)
             x = random.randint(1, count - randn)
     else:
+        print("ayeee")
         randn = random.randint(1, count)
-        x = random.randint(1, count - randn)
+        x = random.randint(1, count - randn +1)
 
 
 
