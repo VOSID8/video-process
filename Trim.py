394db7a4ff1ef41d1cf9bc98d5766bed458e716e
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2022-12-26 04:11:07 +0530

    Final of frame generation

--- Trim.py
+++ Trim.py
@@ -23,43 +23,44 @@ for i in range(num_rows):
         while flag<3:
             while flag2 == 0:
                 randn = random.randint(1, count)
-                x = random.randint(0, count - randn + 1)
-                start = df.iloc[i, 2*randn]
-                end = int(df.iloc[i, 2*randn + 2*x]) - start
-                if(end - start > 64):
-                    print(df.iloc[i,1])
+                x = random.randint(1, count - randn + 1)
 
-
-                    list = [c,df.iloc[i,1],f'{i}'+'_'+f'{flag}'+df.iloc[i,2],x]
-                    for j in range(start, end):
-                        print(j)
-                        list.append(df.iloc[i,j])
-
-                    #df1 = pd.DataFrame([[c,df.iloc[i,1],f'{i}'+f'{flag}'+df.iloc[i,2],1,0,end]],
-                    #                   columns=['sno','type', 'name', 'count','L1','L2'])
+                list = [c, df.iloc[i, 1], f'{i}' + '_' + f'{flag}' + df.iloc[i, 2], x]
+                start = df.iloc[i, 2 * randn + 1]
+                end = df.iloc[i, 2 * randn + 2 * x]
+                for jj in range(1, 2 * x + 1):
+                    list.append(df.iloc[i, 2 * randn + jj])
+                if (int(end) - int(start) > 64):
+                    c += 1
                     flag += 1
                     flag2 = 1
-                    c +=1
-                    df = pd.concat([df, df1], ignore_index=True, sort=False)
+                    with open('data.csv', 'a', newline='') as csv_file:
+                        writer = csv.writer(csv_file)
+                        writer.writerow(list)
                 else:
                     continue
+            flag2=0
     elif(high_val > 500 and high_val < 1000):
         while flag<2:
             while flag2 == 0:
                 randn = random.randint(1, count)
-                x = random.randint(0, count - randn + 1)
-                start = df.iloc[i, 2*randn]
-                end = int(df.iloc[i, 2*randn + 2*x]) - start
-                if (end - start > 64):
-                    df1 = pd.DataFrame([[c,df.iloc[i, 1], f'{i}' + f'{flag}'+df.iloc[i, 2], 1, 0, end]],
-                                    columns=['sno','type', 'name', 'count', 'L1', 'L2'])
-                    df = pd.concat([df, df1], ignore_index=True, sort=False)
+                x = random.randint(1, count - randn + 1)
+
+                list = [c, df.iloc[i, 1], f'{i}' + '_' + f'{flag}' + df.iloc[i, 2], x]
+                start = df.iloc[i, 2 * randn + 1]
+                end = df.iloc[i, 2 * randn + 2 * x]
+                for jj in range(1, 2 * x + 1):
+                    list.append(df.iloc[i, 2 * randn + jj])
+                if (int(end) - int(start) > 64):
+                    c += 1
                     flag += 1
                     flag2 = 1
-                    c +=1
+                    with open('data.csv', 'a', newline='') as csv_file:
+                        writer = csv.writer(csv_file)
+                        writer.writerow(list)
                 else:
                     continue
-
+            flag2=0
     else:
         while flag2 == 0:
             randn = random.randint(1, count)
@@ -79,7 +80,6 @@ for i in range(num_rows):
             else:
                 continue
 
-#result.to_csv('RESULT.csv', index=False)
 
 
 
