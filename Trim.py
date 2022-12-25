fba596c96745cfa7df2501b44bbfde578b502785
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2022-12-25 21:01:12 +0530

    Final

--- Trim.py
+++ Trim.py
@@ -3,11 +3,15 @@ import random
 
 # Read the CSV file
 df = pd.read_csv('data.csv')
-new = [1, 2, 3]
+
+#df1 = pd.DataFrame([[1, 2, 3],[5,6,7]], columns=['count','name','type'])
+#result = pd.concat([df, df1], ignore_index=True, sort=False)
+#print(result)
 #df = df.append(pd.Series(new, index=df.columns[:len(new)]), ignore_index=True)
 
 # Print the dataframe
 num_rows = df.shape[0]
+c = num_rows
 
 for i in range(num_rows):
     count = df['count'][i]
@@ -16,29 +20,42 @@ for i in range(num_rows):
     flag = 0
     if(high_val > 1000):
         while flag<3:
-            print("wassup")
             randn = random.randint(1, 2*count)
             x = random.randint(1, 2*count - randn)
             start = df.iloc[i, randn]
-            end = df.iloc[i, randn + x]
+            end = int(df.iloc[i, randn + x]) - start
             if(end - start > 64):
-
+                print(df.iloc[i,1])
+                df1 = pd.DataFrame([[c,df.iloc[i,1],f'{i}'+f'{flag}'+df.iloc[i,2],1,0,end]],
+                                   columns=['sno','type', 'name', 'count','L1','L2'])
                 flag += 1
+                c +=1
+                df = pd.concat([df, df1], ignore_index=True, sort=False)
             else:
                 continue
     elif(high_val > 500 and high_val < 1000):
         while flag<2:
-            print("hohohoho")
             randn = random.randint(1, count)
             x = random.randint(1, count - randn)
+            start = df.iloc[i, randn]
+            end = int(df.iloc[i, randn + x]) -start
+            df1 = pd.DataFrame([[c,df.iloc[i, 1], f'{i}' + f'{flag}'+df.iloc[i, 2], 1, 0, end]],
+                               columns=['sno','type', 'name', 'count', 'L1', 'L2'])
+            df = pd.concat([df, df1], ignore_index=True, sort=False)
+            flag += 1
+            c +=1
+
     else:
-        print("ayeee")
         randn = random.randint(1, count)
-        x = random.randint(1, count - randn +1)
+        x = random.randint(1, count - randn + 1)
+        start = df.iloc[i, randn]
+        end = int(df.iloc[i, randn + x]) - start
+        df1 = pd.DataFrame([[c ,df.iloc[i, 1], f'{i}' + df.iloc[i, 2], 1, 0, end]],
+                           columns=['sno','type', 'name', 'count', 'L1', 'L2'])
+        df = pd.concat([df, df1], ignore_index=True, sort=False)
+        c += 1
 
-
-
-
+df.to_csv('RESULT.csv', index=False)
 
 
 
